9e9ab6cad6fffcb37fa09646255c3349f84a31ad
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-08-08 14:52:53 +0330

    fixed api

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,7 +4,7 @@ import prisma from "@/lib/prisma";
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
-    const {id, first_name, last_name, username} = body
+    const {id, first_name, last_name, username, phone_number} = body
     // @ts-ignore
     let user = await prisma.User.findUnique(
         {
@@ -22,7 +22,7 @@ export async function POST(request: NextRequest) {
                     firstName: first_name || null,
                     lastName: last_name || null,
                     username: username || null,
-                    phoneNumber: null,
+                    phoneNumber: phone_number || null,
                     licenseToken: null,
                 },
             }
