953b08ee568961a7ebf2c0bca364b97a28cdc968
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-08-13 21:05:55 +0330

    fixed bot & app

--- bot/index.ts
+++ bot/index.ts
@@ -1,6 +1,7 @@
 import {Bot, GrammyError, HttpError, InlineKeyboard, Keyboard} from "grammy";
-import axios from "axios";
 import * as dotenv from "dotenv"
+import {axiosNoAuth} from "../lib/axios";
+
 dotenv.config();
 
 
@@ -15,21 +16,8 @@ bot.on("message", async (ctx: any) => {
 });
 
 
-
 // Start the bot.
 bot.start();
-
-
-
-
-
-
-
-
-
-
-
-
 
 bot.catch((err) => {
     const ctx = err.ctx;
@@ -47,80 +35,45 @@ bot.catch((err) => {
 const _checkStatus = async (ctx: any) => {
     if (ctx.from.is_bot) return
 
-    console.log("hjeee")
 
-    ctx.reply("heyyyyyyyyyy youuuuuuu")
-
-    return
     const data = {
         id: ctx.from.id,
-        first_name: ctx.from.first_name,
-        last_name: ctx.from.last_name,
+        firstName: ctx.from.first_name,
+        lastName: ctx.from.last_name,
         username: ctx.from.username,
     }
-    const {data: result} = await axios.post("https://classregbot.mentorader.ir/api/user", data)
-    if (result.ok) {
-        console.log(result.user, "###################################")
-        if (!!result.user.licenseToken) {
-            return await _showLicense(ctx)
-        } else if (!!result.user.paid) {
-            return await _generateLicense(ctx)
-        } else if (!!result.user.phoneNumber) {
-            return await _showMiniApp(ctx)
-        } else if (!!ctx.message.contact) {
-            return await _registerComplete(ctx)
-        } else {
-            return await _sendNumberRequest(ctx)
-        }
+    const {data: result} = await axiosNoAuth.post("user", data)
+
+    if (!!result.user.phoneNumber) {
+        return await showMainMenu(ctx)
+    } else if (!!ctx.message.contact) {
+        return await savePhoneNumber(ctx)
+    } else if (!result.user.phoneNumber) {
+        return await sendPhoneNumberRequest(ctx)
     }
 }
 
 
-const _generateLicense = async (ctx: any) => {
-    const {data: result} = await axios.post("https://classregbot.mentorader.ir/api/license/" + ctx.from.id)
-
-    if(!result.ok) {
-        return await ctx.api.sendMessage(ctx.chat.id, "خطایی رخ داده است :(")
-    }
-
-    if(!result.user.licenseToken) {
-        return await ctx.api.sendMessage(ctx.chat.id, "خطایی در تولید لایسنس رخ داد، مجدد تلاش کنید")
-    }
-
-    const text = "لایسنس شما در اسپات پلیر:\n" + "```" + result.user.licenseToken + "```" + "\n\n لایسنس را کپی کرده و در اسپات پلیر درج کنید"
-    await ctx.api.sendMessage(ctx.chat.id, text, {parse_mode: "MarkdownV2"})
-}
-
-
-const _showLicense = async (ctx: any) => {
-    const {data: result} = await axios.get("https://classregbot.mentorader.ir/api/user/" + ctx.from.id)
-    const text = "لایسنس شما در اسپات پلیر:\n" + "```" + result.user.licenseToken + "```" + "\n\n لایسنس را کپی کرده و در اسپات پلیر درج کنید"
-    await ctx.api.sendMessage(ctx.chat.id, text, {parse_mode: "MarkdownV2"})
-}
-
-
-const _showMiniApp = async (ctx: any) => {
+const showMainMenu = async (ctx: any) => {
     const text = "جهت ثبت نام در کلاس روی دکمه زیر کلیک کنید"
     const keyboard = new InlineKeyboard()
         .webApp("ثبت نام", "https://classregbot.mentorader.ir/")
     await ctx.api.sendMessage(ctx.chat.id, text, {parse_mode: "MarkdownV2", reply_markup: keyboard})
 }
 
-const _registerComplete = async (ctx: any) => {
+const savePhoneNumber = async (ctx: any) => {
     const data = {
         id: ctx.from.id,
-        first_name: ctx.from.first_name,
-        last_name: ctx.from.last_name,
+        firstName: ctx.from.first_name,
+        lastName: ctx.from.last_name,
         username: ctx.from.username,
-        phone_number: ctx.message.contact.phone_number
+        phoneNumber: ctx.message.contact.phone_number
     }
-    const {data: result} = await axios.post("https://classregbot.mentorader.ir/api/user", data)
+    await axiosNoAuth.post("user", data)
 
-    // await ctx.api.editMessageReplyMarkup(ctx.chat.id, ctx.message.reply_to_message.message_id)
     const keyboard = new Keyboard()
-        .text("ثبت نام")
+        .text("منوی اصلی")
         .resized()
-
 
     const text = "ثبت نام شما با موفقیت انجام شد"
     await ctx.api.sendMessage(ctx.chat.id, text, {reply_markup: keyboard})
@@ -131,15 +84,13 @@ const _registerComplete = async (ctx: any) => {
 }
 
 
-const _sendNumberRequest = async (ctx: any) => {
-    const text = "برای استفاده از ربات، روی ارسال شماره موبایل بزنید و سپس Ok را بزنید:"
+const sendPhoneNumberRequest = async (ctx: any) => {
+    const text = "برای استفاده از ربات، روی ارسال شماره موبایل بزنید و سپس Share را بزنید:"
     const keyboard = new Keyboard()
         .requestContact("ارسال شماره موبایل")
         .resized()
-
     keyboard.one_time_keyboard = true
-
-    await ctx.api.sendMessage(ctx.chat.id, text, {parse_mode: "MarkdownV2", reply_markup: keyboard})
+    await ctx.api.sendMessage(ctx.chat.id, text, {reply_markup: keyboard})
 }
 
 
