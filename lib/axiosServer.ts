397a4b7c4a57cdae3a2349aa7828626fecaa9480
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-08-13 21:50:38 +0330

    fixed bot & app

---
+++ lib/axiosServer.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+
+
+const config = {
+    baseURL: `${process.env.NEXT_PUBLIC_CORE_BASE_URL}`,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    // withCredentials: true,
+    validateStatus: function (status: number) {
+        return status >= 200 && status < 400;
+    },
+    paramsSerializer: {
+        indexes: null // by default: false
+    },
+}
+
+
+// **********************
+// axios server
+const axiosServer = axios.create(config)
+axiosServer.interceptors.response.use(
+    (response: any) => {
+        return response;
+    },
+    (error: any) => {
+        return Promise.reject(error.response.data)
+    },
+);
+export {axiosServer}
+
